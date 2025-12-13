1cefe7194ebabea8889503e2359bd3f4ef7ec2ce
SerKo <serko.dev@gmail.com>
2025-12-13 21:28:42 +0800

    feat: add retry

--- tests/vendor.spec.ts
+++ tests/vendor.spec.ts
@@ -7,7 +7,7 @@ const VENDOR_LIST: { repo: string; path: string }[] = [
 test.skipIf(process.env.TEST_VENDOR !== '1')(`ensure vendor is updated`, async () => {
 	const promises = VENDOR_LIST.map(async item => ({
 		...item,
-		commit: await getRemoteCommit(item.repo, item.path),
+		commit: await retry(() => getRemoteCommit(item.repo, item.path)),
 	}));
 	const snapshot = await Promise.all(promises);
 	expect(snapshot).toMatchSnapshot();
@@ -17,5 +17,25 @@ async function getRemoteCommit(repo: string, path: string): Promise<string | und
 	console.log('fetching', repo, path);
 	const response = await fetch(`https://api.github.com/repos/${repo}/commits?path=${path}&per_page=1`);
 	const data = await response.json();
-	return data[0]?.sha || '';
+	const sha: string | undefined = data[0]?.sha;
+	if (!sha) {
+		throw new Error(`No commits found for ${repo}/${path}`);
+	}
+	return sha;
+}
+
+async function retry<T>(fn: () => Promise<T>, retries = 3, delay = 1000): Promise<T> {
+	try {
+		return await fn();
+	}
+	catch (error) {
+		if (retries > 0) {
+			console.warn(`Retrying... (${retries} left)`);
+			await new Promise(res => setTimeout(res, delay));
+			return retry(fn, retries - 1, delay);
+		}
+		else {
+			throw error;
+		}
+	}
 }
